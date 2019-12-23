5507a678590cee7d1347424ca84c685b03fa6464
Your Name <you@example.com>
2019-12-23 15:54:09 -0800

    moved onClick to overlay

--- food-app/src/components/Moods.js
+++ food-app/src/components/Moods.js
@@ -9,32 +9,32 @@ const Moods = (props) => {
             <div class="moods">
                 <div className="container">
                     <img className="image"  src="https://res.cloudinary.com/dfulxq7so/image/upload/v1576008467/iconfinder_1_Emoji_happy_smiley_surprised_emoticon_3129756_fxxwqb.png"/>
-                    <div className="overlay">
-                        <div className="text" onClick={() => props.setAwesomeMood(true)}>awesome</div>
+                    <div className="overlay" onClick={() => props.setAwesomeMood(true)}>
+                        <div className="text">awesome</div>
                     </div>
                 </div>
                 <div className="container">
                     <img className="image" src="https://res.cloudinary.com/dfulxq7so/image/upload/v1576008440/iconfinder_7_Excited_laughing_smiley_happy_joyful_3129753_yron8f.png"/>
-                    <div className="overlay">
-                        <div className="text" onClick={() => props.setGreatMood(true)} >great</div>
+                    <div className="overlay" onClick={() => props.setGreatMood(true)}>
+                        <div className="text">great</div>
                     </div>
                 </div>
                 <div className="container">
                     <img className="image" src="https://res.cloudinary.com/dfulxq7so/image/upload/v1576008436/iconfinder_9_Emoji_happy_smiley_surprised_emoticon_3129755_b1ykdv.png"/>
-                    <div className="overlay">
-                        <div className="text" onClick={() => props.setOkayMood(true)}>okay</div>
+                    <div className="overlay" onClick={() => props.setOkayMood(true)}>
+                        <div className="text">okay</div>
                     </div>
                 </div>
                 <div className="container">
                     <img className="image" src="https://res.cloudinary.com/dfulxq7so/image/upload/v1576009802/iconfinder_4_Emoji_exhausted_emoticon_tired_emoji_tired_face_3129749_1_kntuhd.png"/>
-                    <div className="overlay">
-                        <div className="text" onClick={() => props.setEhhMood(true)} >ehh..</div>
+                    <div className="overlay" onClick={() => props.setEhhMood(true)}>
+                        <div className="text">ehh..</div>
                     </div>
                 </div>
                 <div className="container">
                     <img className="image" src="https://res.cloudinary.com/dfulxq7so/image/upload/v1576008442/iconfinder_11_Emoticons_smiley_laughing_face_face_smiley_laughing_tears_3129752_sinxh9.png"/>
-                    <div className="overlay">
-                        <div className="text" onClick={() => props.setBadMood(true)}>bad!</div>
+                    <div className="overlay" onClick={() => props.setBadMood(true)}>
+                        <div className="text">bad!</div>
                     </div>
                 </div>
             </div>
